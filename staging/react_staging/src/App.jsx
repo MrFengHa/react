69537dae2e31c5055383117113aa03cd789d33ae
MrFengHa <1109179567@qq.com>
2021-05-25 10:40:22 +0800

    NavLink的基本使用

--- staging/react_staging/src/App.jsx
+++ staging/react_staging/src/App.jsx
@@ -1,6 +1,8 @@
-import {Link, BrowserRouter, Route} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
+import {Link, BrowserRouter, Route, NavLink} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from "./components/Header";
+import React from "react";
 
 export default function App() {
     return (
@@ -8,7 +10,7 @@ export default function App() {
         <div>
             <div className="row">
                 <div className="col-xs-offset-2 col-xs-8">
-                    <div className="page-header"><h2>React Router Demo</h2></div>
+                    <Header a={1}></Header>
                 </div>
             </div>
             <div className="row">
@@ -17,8 +19,8 @@ export default function App() {
                         {/*<a className="list-group-item active" href="./about.html">About</a>*/}
                         {/*<a className="list-group-item" href="./home.html">Home</a>*/}
                         {/*在rect中靠路由连接实现切换组件*/}
-                        <Link className="list-group-item" to="/about">About</Link>
-                        <Link className="list-group-item" to="/home">Home</Link>
+                        <NavLink className="list-group-item" to="/about">About</NavLink>
+                        <NavLink className="list-group-item" to="/home">Home</NavLink>
                     </div>
                 </div>
                 <div className="col-xs-6">
---
+++ staging/react_staging/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header (props){
+
+    return(
+
+        <div className="page-header"><h2>React Router Demo{props.a}</h2></div>
+    )
+}
