ad322423a4f445adf1c4508aa759e7f766d2fb93
MrFengHa <1109179567@qq.com>
2021-05-25 10:07:07 +0800

    路由的基本使用

--- staging/react_staging/05_src_github搜索案例_axios/index.js
+++ staging/react_staging/05_src_github搜索案例_axios/index.js
@@ -4,6 +4,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //引入App
 import App from './App'
-import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
