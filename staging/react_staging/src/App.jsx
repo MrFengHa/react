3ca9c84e415e3a9ecf6e5818a1d5d24b77e59722
MrFengHa <1109179567@qq.com>
2021-05-26 16:43:25 +0800

    Redux

--- staging/react_staging/src/App.jsx
+++ staging/react_staging/src/App.jsx
@@ -1,38 +1,11 @@
-import {Link, BrowserRouter, Route, NavLink} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Header from "./components/Header";
 import React from "react";
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
 
 export default function App() {
     return (
-
         <div>
-            <div className="row">
-                <div className="col-xs-offset-2 col-xs-8">
-                    <Header a={1}></Header>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-xs-2 col-xs-offset-2">
-                    <div className="list-group">
-                        {/*<a className="list-group-item active" href="./about.html">About</a>*/}
-                        {/*<a className="list-group-item" href="./home.html">Home</a>*/}
-                        {/*在rect中靠路由连接实现切换组件*/}
-                        <NavLink className="list-group-item" to="/about">About</NavLink>
-                        <NavLink className="list-group-item" to="/home">Home</NavLink>
-                    </div>
-                </div>
-                <div className="col-xs-6">
-                    <div className="panel">
-                        <div className="panel-body">
-                            {/*注册路由*/}
-                            <Route path="/about" component={About}/>
-                            <Route path="/home" component={Home}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Button type="primary">Primary Button</Button>
         </div>
     )
 }
--- staging/react_staging/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header (props){
-
-    return(
-
-        <div className="page-header"><h2>React Router Demo{props.a}</h2></div>
-    )
-}
